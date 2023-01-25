c8bb1d9e4ae904905890b10f8e0b6794a71313f2
Gu <bing.q.gu@vanderbilt.edu>
2023-01-25 15:55:50 -0600

    initial setup

---
+++ voice-of-vandy/components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div>I am a Sidebar</div>
+  )
+}
+
+export default Sidebar
---
+++ voice-of-vandy/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Voice of Vandy</title>
+      </Head>
+      
+      <h1>Hello World</h1>
+    </>
+  )
+}
