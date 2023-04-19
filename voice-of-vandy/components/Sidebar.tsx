ea79f840790b9012840bf0d9848dc226d9279c1b
Michelle Zhu <michelle.l.zhu@vanderbilt.edu>
2023-04-19 15:03:22 -0500

    blah

--- voice-of-vandy/components/Sidebar.tsx
+++ voice-of-vandy/components/Sidebar.tsx
@@ -14,21 +14,25 @@ function Sidebar() {
 
   return (
     <div className = "flex flex-col col-span-2 items-center px-4 md:items-start">
-      <img className="m-3 h-10 w-10" src = "https://fox17.com/resources/media2/16x9/full/1015/center/80/6ce69a1a-c627-4a26-8113-981281ef3682-large16x9_Vandyathletics.png"
+      <p className="font-bold text-5xl justify-center text-center text-FlatGold m-6">Voice of Vandy</p>
+      <img className="m-10 h-30 w-30" 
+        src="https://upload.wikimedia.org/wikipedia/commons/6/61/Vanderbilt_Commodores_logo.svg"
       alt="" />
 
       {/* <SidebarRow Icon={HomeIcon} title = "Home" /> */}
       {/* <SidebarRow Icon={UserIcon} title = "Profile" />
       <SidebarRow Icon={ChatIcon} title = "Chats" /> */}
 
-      <div className="absolute inset-y-60 h-16 w-60">
-        <div className='m-4 h-100 w-60 text-3xl'>
+      <div className="relative inset-y-30 h-16 w-60">
+        <p className='m-4 h-100 w-60 text-3xl'>
           <u>Daily Prompt:</u>
-        </div>
+        </p>
 
-        <div className='m-5 h-10 w-40 text-center'>
-        Give us your best SpongeBob impression!
-        </div>
+        <p className='m-5 h-10 w-40 text-center'>
+          <i>
+            Give us your best SpongeBob impression!
+          </i>
+        </p>
       </div>
 
       <div className="absolute bottom-0 left-45 h-16 w-60">
