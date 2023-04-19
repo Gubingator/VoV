bf156b75abae1ee3151823753e4038d2d15388ee
Gubingator <bing@Bings-MacBook-Air.local>
2023-04-19 16:35:46 -0500

    trying to fix deployment from fetchTweets

--- voice-of-vandy/utils/fetchTweets.ts
+++ voice-of-vandy/utils/fetchTweets.ts
@@ -3,7 +3,7 @@ import { Tweet } from "@/typings";
 // REST API call to our back end
 
 export const fetchTweets = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getTweets`)
+    const res = await fetch(`/api/getTweets`)
 
     const data = await res.json();
     const tweets: Tweet[] = data.tweets;
